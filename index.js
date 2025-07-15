b57c0d012383da38ad003113ae293f876ca61be4
Oasis777999 <collegeoasis915@gmail.com>
2025-07-15 15:29:33 +0530

    Product box content integreaded

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.port;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 const productRoute = require("./routes/product");
 const customerRoute = require("./routes/customer");
--- model/productModel.js
+++ model/productModel.js
@@ -1,54 +1,62 @@
 const mongooose = require("mongoose");
 
-const productSchema = new mongooose.Schema({
-    name:{
-        type:String
+const productSchema = new mongooose.Schema(
+  {
+    name: {
+      type: String,
     },
-    category:{
-        type:String
+    category: {
+      type: String,
     },
-    brand:{
-        type:String
+    brand: {
+      type: String,
     },
-    sku:{
-        type:String
+    sku: {
+      type: String,
     },
-    shortDesc:{
-        type:String
+    shortDesc: {
+      type: String,
     },
-    status:{
-        type:Boolean
+    status: {
+      type: Boolean,
     },
-    costPrice:{
-        type:String
+    costPrice: {
+      type: String,
     },
-    sellPrice:{
-        type:String
+    sellPrice: {
+      type: String,
     },
-    sequence:{
-        type:String
+    sequence: {
+      type: String,
     },
-    length:{
-        type:Number
+    length: {
+      type: Number,
     },
-    breadth:{
-        type:Number
+    breadth: {
+      type: Number,
     },
-    height:{
-        type:Number
+    height: {
+      type: Number,
     },
-    weight:{
-        type:Number
+    weight: {
+      type: Number,
     },
-    colour:{
-        type:String
+    colour: {
+      type: String,
     },
-    warranty:{
-        type:String
+    warranty: {
+      type: String,
     },
-    heroImage:{
-        type:String
-    }
-}, { timestamps: true })
+    boxContent: {
+      type: [String],
+      default: [],
+    },
+    heroImage: {
+      type: [String], // array of image URLs or filenames
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongooose.model("product", productSchema);
